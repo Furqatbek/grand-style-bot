520d3c46f15354143b0bbc25d659d64a82189918
Furqatbek <43913486+Furqatbek@users.noreply.github.com>
2021-01-09 14:28:20 +0300

    Add files via upload

--- kb.js
+++ kb.js
@@ -15,70 +15,12 @@ module.exports = {
         orqaga: '🔙 Orqaga',
     },
 
-    inlinekeyboardtime:
-    [
-        {
-            text: '9:00',
-            callback_data: '#'
-        },
-        {
-            text: '10:00',
-            callback_data: '#'
-        },
-        {
-            text: '11:00',
-            callback_data: '#'
-        },
-        {
-            text: '12:00',
-            callback_data: '#'
-        },
-        {
-            text: '14:00',
-            callback_data: '#'
-        },
-        {
-            text: '15:00',
-            callback_data: '#'
-        },
-        {
-            text: '16:00',
-            callback_data: '#'
-        },
-        {
-            text: '17:00',
-            callback_data: '#'
-        },
-        {
-            text: '18:00',
-            callback_data: '#'
-        },
-        {
-            text: '19:00',
-            callback_data: '#'
-        },
-        {
-            text: '20:00',
-            callback_data: '#'
-        },
-        {
-            text: '21:00',
-            callback_data: '#'
-        },
-    ],
 
     hizmatlar: {
-        hizmat1: 'hizmat1',
-        hizmat01: '🔴hizmat1',
-        hizmat2: 'hizmat2',
-        hizmat02: '🔴hizmat2',
-        hizmat3: 'hizmat3',
-        hizmat03: '🔴hizmat3',
-        hizmat4: 'hizmat4',
-        hizmat04: '🔴hizmat4',
-        hizmat5: 'hizmat5',
-        hizmat05: '🔴hizmat5',
-        enough: 'Navbat olish',
+        hizmat1: '👨‍⚖️Men bolajak kuyovman!',
+        hizmat2: '🙂Yuz tozalash',
+        hizmat3: '💇‍♂️Kantovka',
+        hizmat4: '🙃Oddiy soch olish',
         orqaga: '🔙 Orqaga',
     },
 
--- keyboard.js
+++ keyboard.js
@@ -1,187 +1,29 @@
 const kb = require('./kb');
 module.exports = {
-home: [
-    [kb.home.navbat],
-    [kb.home.savollar, kb.home.contact],
-],
+    home: [
+        [kb.home.navbat],
+        [kb.home.savollar],
+        [kb.home.contact]
+    ],
 
-ustatanlash: [
-    [kb.ustatanlash.bobur],
-    [kb.ustatanlash.orqaga]
-],
+    ustatanlash: [
+        [kb.ustatanlash.bobur],
+        [kb.ustatanlash.orqaga]
+    ],
 
-savollar: [
-    [kb.savollar.orqaga]
-],
+    savollar: [
+        [kb.savollar.orqaga]
+    ],
 
- // inline keyboard 
 
- /*
- inlinekeyboard: 
- [
-     [
-         {
-             text: 'Oktyabr 2020',
-             callback_data: '#'
-         }
-     ],
-     [
-         {
-             text: '1',
-             callback_data: 'kb.inlinekeyboardtime'
-         },
-         {
-             text: '2',
-             callback_data: '#'
-         },
-         {
-             text: '3',
-             callback_data: '#'
-         },
-         {
-             text: '4',
-             callback_data: '#'
-         },
-         {
-             text: '5',
-             callback_data: '#'
-         },
-         {
-             text: '6',
-             callback_data: '#'
-         },
-         {
-             text: '7',
-             callback_data: '#'
-         },
-     ],
-     [
-         {
-             text: '8',
-             callback_data: '#'
-         },
-         {
-             text: '9',
-             callback_data: '#'
-         },
-         {
-             text: '10',
-             callback_data: '#'
-         },
-         {
-             text: '11',
-             callback_data: '#'
-         },
-         {
-             text: '12',
-             callback_data: '#'
-         },
-         {
-             text: '13',
-             callback_data: '#'
-         },
-         {
-             text: '14',
-             callback_data: '#'
-         },
-     ],
-     [
-         {
-             text: '15',
-             callback_data: '#'
-         },
-         {
-             text: '16',
-             callback_data: '#'
-         },
-         {
-             text: '17',
-             callback_data: '#'
-         },
-         {
-             text: '18',
-             callback_data: '#'
-         },
-         {
-             text: '19',
-             callback_data: '#'
-         },
-         {
-             text: '20',
-             callback_data: '#'
-         },
-         {
-             text: '21',
-             callback_data: '#'
-         },
-     ],
-     [
-         {
-             text: '22',
-             callback_data: '#'
-         },
-         {
-             text: '23',
-             callback_data: '#'
-         },
-         {
-             text: '24',
-             callback_data: '#'
-         },
-         {
-             text: '25',
-             callback_data: '#'
-         },
-         {
-             text: '26',
-             callback_data: '#'
-         },
-         {
-             text: '27',
-             callback_data: '#'
-         },
-         {
-             text: '28',
-             callback_data: '#'
-         },
-     ],
-     [
-         {
-             text: '29',
-             callback_data: '#'
-         },
-         {
-             text: '30',
-             callback_data: '#'
-         },
-         {
-             text: '31',
-             callback_data: '#'
-         },
-     ],
-     [
-         {
-             text: '<<',
-             callback_data: '#'
-         },
-         {
-             text: '>>',
-             callback_data: '#'
-         },
-     ]
- ],
- */
+    hizmat: [
+        [kb.hizmatlar.hizmat1],
+        [kb.hizmatlar.hizmat2],
+        [kb.hizmatlar.hizmat3],
+        [kb.hizmatlar.hizmat4],
+        [kb.hizmatlar.orqaga]
 
- hizmat : [
-     [kb.hizmatlar.hizmat1],
-     [kb.hizmatlar.hizmat2],
-     [kb.hizmatlar.hizmat3],
-     [kb.hizmatlar.hizmat4],
-     [kb.hizmatlar.hizmat5],
-     [kb.hizmatlar.enough],
-     [kb.hizmatlar.orqaga]
-
- ]
+    ]
 
 
 
